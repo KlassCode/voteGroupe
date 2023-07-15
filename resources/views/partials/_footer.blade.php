bec46cdd4e8fe58450cbe4d87cd0c81d74fe0a0a
Klassen Edmond <klassenedmond7@gmail.com>
2023-07-15 14:23:12 -0400

    project initialisation

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,70 @@
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+
+  <div class="container">
+    <div class="row gy-4">
+      <div class="col-lg-5 col-md-12 footer-info">
+        <a href="index.html" class="logo d-flex align-items-center">
+          <span class="footer-title">VoteGroupe</span>
+        </a>
+        <p>Organisez des élections pour désigner des représentants, prendre des décisions sur les projets de classe ou voter sur des sujets d'importance.</p>
+        <div class="social-links d-flex mt-4">
+          <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+          <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-2 col-6 footer-links">
+        <h4 class="footer-title">Useful Links</h4>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">A propos</a></li>
+          <li><a href="#">Communautes</a></li>
+          <li><a href="#">Conditions d'utilisation</a></li>
+          <li><a href="#">Politique de confidentialité</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-2 col-6 footer-links">
+        <h4 class="footer-title">Communautés Actifs</h4>
+        <ul>
+          <li><a href="#">College Marie Regine</a></li>
+          <li><a href="#">Universite Inuka</a></li>
+          <li><a href="#">Product Management</a></li>
+          <li><a href="#">Sogebank Selling team</a></li>
+          <li><a href="#">Graphic Design Bnc</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+        <h4 class="footer-title">Contact Us</h4>
+        <p>
+          55 bizoton 61 Carrefour <br>
+          Port-au-prince, OUEST<br>
+           <br>HAITI<br>
+          <strong>Phone:</strong> +509 4759 3979<br>
+          <strong>Email:</strong> votegroupe@gmail.com<br>
+        </p>
+
+      </div>
+
+    </div>
+  </div>
+
+  <div class="container mt-4">
+    <div class="copyright">
+      &copy; Copyright <strong><span>votegroupe</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/impact-bootstrap-business-website-template/ -->
+      Designed by <a href="">klasscode</a>
+    </div>
+  </div>
+
+</footer><!-- End Footer -->
+<!-- End Footer -->
