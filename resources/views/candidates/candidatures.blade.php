d27b86ef95915d17a7b625dbe13e935d25e2eb69
Klassen Edmond <klassenedmond7@gmail.com>
2024-09-19 01:30:55 -0400

    Add Candidate card in Candidature section

--- resources/views/candidates/candidatures.blade.php
+++ resources/views/candidates/candidatures.blade.php
@@ -41,9 +41,9 @@
                                             ><i class="bx bx-edit-alt me-1"></i></a
                                             > --}}
                                             @if ($candidate->participation_confirm)
-                                                <button type="button" class="btn rounded-pill btn-info btn-sm">Voir</button>
+                                                <button type="button" class="btn rounded-pill btn-info btn-sm candidate-view" data-id={{$candidate->id}} data-fullname="{{$candidate->fullname}}" data-email="{{$candidate->email}}" data-participation={{$candidate->participation_confirm}} data-election="{{$candidate->election_id}}">Voir</button>
                                             @else
-                                            <button type="button" class="btn rounded-pill btn-warning btn-sm">Confirmer</button>
+                                            <button type="button" class="btn rounded-pill btn-warning btn-sm candidate-view" data-id={{$candidate->id}} data-fullname="{{$candidate->fullname}}" data-email="{{$candidate->email}}" data-participation={{$candidate->participation_confirm}} data-election="{{$candidate->election_id}}">Confirmer</button>
                                             @endif
                                             
                                             <form action="{{route('election.delete',$candidate->id)}}" method="post">
@@ -67,8 +67,91 @@
                         
                         <!--/ Basic Bootstrap Table -->
                     </div>
-                    <div class="col-lg-6 col-md-4 order-1">
+                    <!--Carte Candidat -->
+                    <div class="col-lg-6 col-md-4 order-1 d-none" id="candidate-card">
+                        <div class="card text-center">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <h5 class="card-header text-start">Ma Carte Candidat <span class="badge rounded-pill bg-label-success me-1">Ouvert</span></h5></div>
+                                <div class="col-lg-6 d-flex mt-4 pt-1">
+                                    <div class="avatar flex-shrink-0 me-1">
+                                        <img src="{{asset('assets/img/icons/paypal.png')}}" alt="User" class="rounded" />
+                                    </div>
+                                    <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-1">
+                                        <div class="me-2">
+                                          <small class="text-muted d-block mb-1 text-start">Rang : {{0}}</small>
+                                          <h6 class="mb-0">Qte Votes</h6>
+                                        </div>
+                                        <div class="user-progress d-flex align-items-center gap-1">
+                                          <h5 class="mb-0">0</h5>
+                                          <span class="text-muted m-1">Votes</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            
+                            <div class="card-body">
+                                <div class="d-flex align-items-start align-items-sm-center gap-4">
+                                    <img
+                                      src="{{asset('assets/img/dashboard/1.png')}}"
+                                      alt="user-avatar"
+                                      class="d-block rounded"
+                                      height="100"
+                                      width="100"
+                                      id="uploadedAvatar"
+                                    />
+                                    <div class="button-wrapper">
+                                      <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
+                                        <span class="d-none d-sm-block">Ajouter une photo</span>
+                                        <i class="bx bx-upload d-block d-sm-none"></i>
+                                        <input
+                                          type="file"
+                                          id="upload"
+                                          class="account-file-input"
+                                          hidden
+                                          accept="image/png, image/jpeg"
+                                        />
+                                      </label>
+            
+                                      <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
+                                    </div>
+                                  </div>
+                                    {{-- <a href="javascript:void(0)" class="btn btn-primary">Go somewhere</a> --}}
+                            </div>
+                            <hr class="my-0" />
+                            <div class="card shadow-none text-start bg-transparent border border-info m-3">
+                                <div class="card-body">
+                                  <h5 class="card-title">Informations</h5>
+                                  <form id="add-candidate-form">
+                                        @csrf
+                                        <input type="hidden" id="election_id" name="election_id" value="">
+                                        <div class="mb-1">
+                                            <label class="form-label" for="basic-default-email">Election</label>
+                                            <input type="mail" class="form-control" name="election" id="election-title" disabled/>
+                                        </div>
+                                        <div class="mb-1">
+                                            <label class="form-label" for="basic-default-email">Email</label>
+                                            <input type="mail" class="form-control" name="email" id="email" disabled/>
+                                        </div>
+                                        <div class="mb-1">
+                                            <label class="form-label" for="basic-default-fullname">Nom Complet</label>
+                                            <input type="text" class="form-control" name="fullname" id="fullname"/>
+                                        </div>  
+    
+                                        <button type="button" class="btn btn-primary" id="btn-participation-confirm">Je participe</button>
+                                  </form>
+                                </div>
+                            </div>
 
+                            
+
+                            
+
+                            
+                            <div class="card-footer text-muted">2 days ago</div>
+                          </div>
+                        </div>
                     </div>
                 </div>
   
@@ -83,5 +166,30 @@
 </div>
 @endsection
 @push('after_script')
-  
+<script type="text/javascript">
+    $(document).on("click", ".candidate-view", function () {
+        let myCandidateId = $(this).data('id');
+        let myCandidateName = $(this).data('fullname');
+        let myCandidateEmail = $(this).data('email');
+        let myCandidateElection = $(this).data('election');
+        let myCandidateParticipation = $(this).data('participation')
+        
+        if(myCandidateParticipation==0){
+            //give the view where candidate can modify his information and confirm his participation
+            $("#fullname").val(myCandidateName);
+            $("#email").val(myCandidateEmail);
+            $("#election-title").val(myCandidateElection);
+            $("#candidate-card").removeClass("d-none");
+            console.log("Hello")
+        }else{
+            //give the view where candidate can view his progressin in election
+            $("#fullname").val(myCandidateName);
+            $("#email").val(myCandidateEmail);
+            $("#election-title").val(myCandidateElection);
+            $("#candidate-card").removeClass("d-none");
+            console.log("Hello")
+        }
+    });
+    
+</script>
 @endpush
--- resources/views/layouts/app_dashboard.blade.php
+++ resources/views/layouts/app_dashboard.blade.php
@@ -48,26 +48,8 @@
         <script>
           document.write(new Date().getFullYear());
         </script>
-        , made with ❤️ by
-        <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">Forex Code</a>
-      </div>
-      <div>
-        <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-        <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
-
-        <a
-          href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/"
-          target="_blank"
-          class="footer-link me-4"
-          >Documentation</a
-        >
-
-        <a
-          href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
-          target="_blank"
-          class="footer-link me-4"
-          >Support</a
-        >
+         | All right reserved
+        <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">Klass Code</a>
       </div>
     </div>
   </footer>
